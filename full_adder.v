5aacb3a8732203c200f6e33edecd995e029ef557
Ayush Yadav <134876624+ayush-iit-tp@users.noreply.github.com>
2024-12-22 22:36:50 +0530

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -2,3 +2,16 @@ module full_adder(input a, b, cin, output S, Cout);
   assign S = a ^ b ^ cin;
   assign Cout = (a & b) | (b & cin) | (a & cin);
 endmodule
+
+module half_addr(input a, b, output s, c);
+  assign s = a^b;
+  assign c = a & b;
+endmodule
+
+module full_adder(input a, b, cin, output s_out, c_out);
+  wire s, c0, c1;
+  half_addr HA1 (a, b, s, c0);
+  half_addr HA2 (s, cin, s_out, c1);
+  
+  assign c_out = c0 | c1;
+endmodule
